4224a0f5b25db7059d790127bbf472d5590400f8
Donny Greenberg <15992114+dongreenberg@users.noreply.github.com>
2025-12-17 11:46:25 -0500

    Rename Sync to Store (#1932)

--- python_client/kubetorch/data_store/__init__.py
+++ python_client/kubetorch/data_store/__init__.py
@@ -1,18 +1,18 @@
 """
-Data sync utilities for kubetorch.
+Data store utilities for kubetorch.
 
 This package provides a key-value store interface for transferring data to and from the cluster.
 It contains the core rsync functionality used throughout kubetorch.
 """
 
-from .data_sync_client import DataSyncClient, DataSyncError
-from .data_sync_cmds import get, ls, put, rm, rsync, rsync_async, sync_workdir_from_store, vput
+from .data_store_client import DataStoreClient, DataStoreError
+from .data_store_cmds import get, ls, put, rm, rsync, rsync_async, sync_workdir_from_store, vput
 from .key_utils import is_service_name, parse_key, ParsedKey
 from .rsync_client import RsyncClient
 
 __all__ = [
-    "DataSyncClient",
-    "DataSyncError",
+    "DataStoreClient",
+    "DataStoreError",
     "ParsedKey",
     "RsyncClient",
     "get",
